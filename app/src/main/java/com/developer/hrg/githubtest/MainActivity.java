3f36ef650dab5f1f3c7fa99f9c72bc07c0aecd1c
hrg.developer <hrg.developer@gmail.com>
2018-04-30 16:55:48 +0430

    new toast

--- app/src/main/java/com/developer/hrg/githubtest/MainActivity.java
+++ app/src/main/java/com/developer/hrg/githubtest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hi there", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "another test", Toast.LENGTH_SHORT).show();
     }
 }

cf0cb86b34eff108c1c6a9cae50283014981be44
hrg.developer <hrg.developer@gmail.com>
2018-04-30 17:02:33 +0430

    bogh

--- app/src/main/java/com/developer/hrg/githubtest/MainActivity.java
+++ app/src/main/java/com/developer/hrg/githubtest/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "hi there", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "another test", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "hi lay", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "bogh bogh", Toast.LENGTH_SHORT).show();
     }
 }
